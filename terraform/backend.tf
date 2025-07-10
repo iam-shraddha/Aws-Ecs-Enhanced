afc0c75c2a6801cd5f84664e0d479bdc1051e0e9
shraddha <162278418+iam-shraddha@users.noreply.github.com>
2025-07-10 17:48:10 +0530

    Update backend.tf

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,9 +1,10 @@
 terraform {
   backend "s3" {
-    bucket         = "terraform-infra-state-ecs-app"
-    key            = "ecs-infra-v2/terraform.tfstate"
+    bucket         = "terraform-infra-state-ecs-app"     
+    key            = "ecs-infra-v2/terraform.tfstate"    
     region         = "ap-south-1"
-    dynamodb_table = "terraform-locks"
+    dynamodb_table = "terraform-locks-ecs-app-v2"        
     encrypt        = true
+    lock_timeout   = "300s"
   }
 }
