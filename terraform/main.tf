124cbc5e6bd33b78efa9834d841d348be147bb04
shraddha <162278418+iam-shraddha@users.noreply.github.com>
2025-07-10 17:36:24 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,7 +2,7 @@ provider "aws" {
   region = var.aws_region
 }
 
-data "aws_availability_zones" "available" {}
+data "aws_availability_zones" "available" {} 
   
 # -----------------------------
 # VPC + Networking
