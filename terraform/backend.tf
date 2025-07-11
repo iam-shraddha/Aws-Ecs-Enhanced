2d8e6a6f91a5c969067d1ac813c4a0a47941ba7c
shraddha <162278418+iam-shraddha@users.noreply.github.com>
2025-07-11 10:36:34 +0530

    Update backend.tf

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket         = "my-ecs-infra-bucket"
-    key            = "my-ecs-app/terraform.tfstate"  # <-- updated key
+    key            = "my-ecs-app/terraform.tfstate"  
     region         = "ap-south-1"
     dynamodb_table = "my-ecs-infra-dynamodb"
   }
